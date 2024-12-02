1dfd4ee2f4a68228bbf208e5f955eadc12725e31
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-12-01 21:27:45 -0800

    Finish the main function in the driver program

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "StringBinaryTree.h"
 using namespace std;
 
+void processChoice(int choice, StringBinaryTree& tree);
+
 int main() {
     StringBinaryTree tree;
 
@@ -25,13 +27,29 @@ int main() {
     int choice;
     do
     {
+        // Display the menu
         cout << "\nMenu Options:\n";
         cout << "1. Add a code\n";
         cout << "2. Delete a code\n";
         cout << "3. Search for a code\n";
         cout << "4. Modify a code\n";
-        cout << "5. Display all codes"
-    }
+        cout << "5. Display all codes in-order\n";
+        cout << "6. Display all codes pre-order\n";
+        cout << "7. Display all codes post-order\n";
+        cout << "8. Exit\n";
+
+        // Process the choice
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        cin.ignore(1000, '\n');
+
+        processChoice(choice, tree);
+    } while (choice != 8);
 
     return 0;
+}
+
+void processChoice(int choice, StringBinaryTree& tree)
+{
+
 }

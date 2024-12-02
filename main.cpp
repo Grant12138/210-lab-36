58f622e62347b0ef9d7314976ab4b886efd6d3a4
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-12-01 21:44:06 -0800

    10-minute update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
+/***************************************************************************
+* COMSC-210 | Lab 36 | Grant Luo
+* Editor: CLion
+* Compiler: Apple clang version 16.0.0
+*/
 #include <iostream>
 #include <fstream>
 #include "StringBinaryTree.h"
 using namespace std;
 
 void processChoice(int choice, StringBinaryTree& tree);
+void print_id(string const& lab_desc);
 
 int main() {
+
+    print_id("")
     StringBinaryTree tree;
 
     try
@@ -127,4 +135,13 @@ void processChoice(int choice, StringBinaryTree& tree)
         default:
             cout << "Invalid choice. Please try again.\n";
     }
+}
+
+void print_id(string const& lab_desc)
+{
+    cout << "\nCOMSC210 | Grant Luo | " << lab_desc << "\n";
+    cout << "Editor: CLion\n";
+    cout << "Compiler: Apple clang version 16.0.0\n";
+    cout << "File: " << __FILE__ << "\n";
+    cout << "Compiled: " << __DATE__ << " at " << __TIME__ << "\n\n";
 }
